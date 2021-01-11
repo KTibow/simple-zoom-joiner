3f43846989b8225c7aa4ea39899082fc397cdc85
Kendell R <KTibow@users.noreply.github.com>
2021-01-11 13:04:17 -0800

    Add tutorial

--- app.py
+++ app.py
@@ -39,6 +39,33 @@ transform: skewX(0deg);
 }
 </style>"""
 
+@app.route("/")
+def tutorial():
+    return """
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
+<style>
+h1, h2 {
+    font-family: Quicksand, sans-serif;
+}
+p {
+    font-family: sans-serif;
+}
+</style>
+<h1 style='color: #84F;'>Simple zoom joiner</h1>
+<p>Join meetings without bloated webpages.</p>
+<h2>/j/&lt;meeting_number&gt;</h2>
+<p>Join plain Zoom meetings, without passwords. Just take the zoom.us/j/&lt;thing&gt; and put it into simple-zoom-joiner.herokuapp.com/j/&lt;thing&gt;.</p>
+<h2>/j/&lt;meeting_number&gt;?pwd=&lt;password&gt;</h2>
+<p>Join with passwords. The password is the encoded thing from zoom.us/j/&lt;thing&gt;?pwd=&lt;thing&gt;.</p>
+<h2>/custom/&lt;meeting_protocol&gt;/&lt;path&gt;</h2>
+<p>Join with custom protocols, like for webex meetings. So wbx://woof would be simple-zoom-joiner.herokuapp.com/custom/wbx/woof.</p>
+<h2>/redirect/&lt;domain_name&gt;/&lt;path&gt;</h2>
+<p>Redirect to websites. So doxy.me/adoctor would be simple-zoom-joiner.herokuapp.com/redirect/doxy.me/adoctor.</p>
+<p>I'm not affiliated with Zoom, Webex, or anything else. What's theirs it theirs; what's mine is mine.</p>
+"""
+
+
 @app.route("/j/<meeting_number>")
 def join_plain(meeting_number):
     return f"""
